8c014a6df8ce1d5c5d2050cbe270e4f4f83f536e
hhvrfn <hhvrfn@outlook.com>
2025-08-22 17:36:21 +0800

    Rename the bot

---
+++ src/main/java/Hhvrfn.java
@@ -0,0 +1,11 @@
+public class Hhvrfn {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm hhvrfn\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+
+
+
